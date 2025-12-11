b5b2c3f086579e627c80e801c6376b75c5b2b1e9
Kit <kit@Kits-MacBook-Pro.local>
2025-12-11 14:50:54 +0000

    Update init to cut back on rand calls to speed up env creation Close #11

--- agent.py
+++ agent.py
@@ -8,7 +8,8 @@ import agent_graphs
 
 # q learning
 
-class Agent():
+
+class Agent:
     def __init__(self, learning_rate, inital_epsilon, epsilon_decay, final_epsilon):
         self.q_values = defaultdict(lambda: np.zeros(2))
         self.learning_rate = learning_rate
@@ -26,28 +27,30 @@ class Agent():
         # missing epsilon = choose best action
         # epsilon decays over time (less exploration)
         self.total_states += 1
-        if (np.array_equal(self.q_values[obs], np.zeros(2))): # new state
+        if np.array_equal(self.q_values[obs], np.zeros(2)):  # new state
             self.new_states += 1
         if random.random() < self.epsilon:
             return round(random.random())
         else:
             return int(np.argmax(self.q_values[obs]))
-    
+
     def update(self, obs, action, reward):
         # difference in expected reward vs. observed reward (naive)
-        error = reward - self.q_values[obs][action] 
+        error = reward - self.q_values[obs][action]
         # update expected reward
-        self.q_values[obs][action] = self.q_values[obs][action] + self.learning_rate * error 
+        self.q_values[obs][action] = (
+            self.q_values[obs][action] + self.learning_rate * error
+        )
 
         self.training_error.append(error)
-    
+
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
-    
+
     def get_best_action(self, obs):
         # no exploration for testing
         return int(np.argmax(self.q_values[obs]))
-    
+
     def get_eval(self, obs):
         # return the agents expected reward for each action
         return self.q_values[obs]
@@ -60,30 +63,46 @@ class Agent():
 
         print("\n\n")
         print("Hard totals:\n  ", end="")
-        [print(" %2i " %x, end="") for x in range(2, 12)]
+        [print(" %2i " % x, end="") for x in range(2, 12)]
         print("")
         for player in range(21, 3, -1):
-            print("%2i" %player, end=" ")
+            print("%2i" % player, end=" ")
             for dealer in range(2, 12):
                 e = self.q_values[(player, 0, dealer)]
-                print(" %s " %(ACTIONS[int(np.argmax(e))] if not np.array_equal(e, np.zeros(2)) else "X"), end=" ")
+                print(
+                    " %s "
+                    % (
+                        ACTIONS[int(np.argmax(e))]
+                        if not np.array_equal(e, np.zeros(2))
+                        else "X"
+                    ),
+                    end=" ",
+                )
             print("")
-        
+
         print("")
         print("Soft totals:\n    ", end="")
-        [print(" %2i " %x, end="") for x in range(2, 12)]
+        [print(" %2i " % x, end="") for x in range(2, 12)]
         print("")
         for player in range(10, 0, -1):
-            print("%2i,A" %player, end=" ")
+            print("%2i,A" % player, end=" ")
             for dealer in range(2, 12):
                 e = self.q_values[(11 + player, 1, dealer)]
-                print(" %s " %(ACTIONS[int(np.argmax(e))] if not np.array_equal(e, np.zeros(2)) else "X"), end=" ")
+                print(
+                    " %s "
+                    % (
+                        ACTIONS[int(np.argmax(e))]
+                        if not np.array_equal(e, np.zeros(2))
+                        else "X"
+                    ),
+                    end=" ",
+                )
             print("")
-    
+
     def reward_function(self, obs, next_obs):
         if next_obs[0] > 21:
             # agent busted (bad)
-            #reward = obs[0] - (next_obs[0] - 21) attempt to make the agent a little less scared of busting
+            # reward = obs[0] - (next_obs[0] - 21) attempt to make the agent a little less scared of busting
             reward = 0
         elif next_obs[2] <= 21 and next_obs[2] > 16 and next_obs[2] > next_obs[0]:
             # agent stood and lost to the dealer (sort of bad)
@@ -97,12 +116,11 @@ class Agent():
         return reward
 
 
-
 LEARNING_RATE = 0.02
 N_EPISODES = 5_000_000
 CHECK_IN = 10
 START_EPSILON = 1.0
-EPSILON_DECAY = START_EPSILON / (N_EPISODES / 4)
+EPSILON_DECAY = START_EPSILON / (N_EPISODES / 2)
 FINAL_EPSILON = 0.1
 N_TESTS = 100_000
 
@@ -111,6 +129,7 @@ win_count = []
 states = []
 checkpoint = int(N_EPISODES / CHECK_IN)
 
+
 def train():
     wins = 0
     losses = 0
@@ -118,12 +137,23 @@ def train():
     tstates = 0
 
     for episode in range(N_EPISODES):
-        env = Environment(0, 0, 8)
+        env = Environment(4)
         obs = env.observe()
         terminated = False
 
         if not (episode % checkpoint) and episode > 0:
-            print("%i: %i / %i = %f; %i / %i = %f" %(episode, wins, checkpoint, wins / checkpoint, agent.new_states - nstates, agent.total_states - tstates, ((agent.new_states - nstates) / (agent.total_states - tstates))))
+            print(
+                "%i: %i / %i = %f; %i / %i = %f"
+                % (
+                    episode,
+                    wins,
+                    checkpoint,
+                    wins / checkpoint,
+                    agent.new_states - nstates,
+                    agent.total_states - tstates,
+                    ((agent.new_states - nstates) / (agent.total_states - tstates)),
+                )
+            )
             win_count.append(wins)
             states.append((agent.new_states - nstates, agent.total_states - tstates))
             wins = 0
@@ -139,10 +169,10 @@ def train():
             agent.update(obs, action, reward)
 
             obs = next_obs
-        
+
         wins += 1 if env.player_has_won() else 0
         losses += 1 if env.player_has_lost() else 0
-        
+
         agent.decay_epsilon()
 
 
@@ -151,7 +181,7 @@ def test():
     losses = 0
 
     for i in range(N_TESTS):
-        env = Environment(0, 0, 8)
+        env = Environment(4)
         obs = env.observe()
         terminated = False
 
@@ -159,11 +189,15 @@ def test():
             action = agent.get_best_action(obs)
             next_obs, terminated = env.step(action)
             obs = next_obs
-        
+
         wins += 1 if env.player_has_won() else 0
         losses += 1 if env.player_has_lost() else 0
 
-    print("TEST: %i / %i = %f; %i / %i = %f" %(wins, N_TESTS, wins / N_TESTS, losses, N_TESTS, losses / N_TESTS))
+    print(
+        "TEST: %i / %i = %f; %i / %i = %f"
+        % (wins, N_TESTS, wins / N_TESTS, losses, N_TESTS, losses / N_TESTS)
+    )
+
 
 def random_test():
     agent = Agent(0, 1, 0, 1)
@@ -171,7 +205,7 @@ def random_test():
     losses = 0
 
     for i in range(N_TESTS):
-        env = Environment(0, 0, 8)
+        env = Environment(4)
         obs = env.observe()
         terminated = False
 
@@ -179,16 +213,19 @@ def random_test():
             action = agent.get_action(obs)
             next_obs, terminated = env.step(action)
             obs = next_obs
-        
+
         wins += 1 if env.player_has_won() else 0
         losses += 1 if env.player_has_lost() else 0
 
-    print("RANDOM TEST: %i / %i = %f; %i / %i = %f" %(wins, N_TESTS, wins / N_TESTS, losses, N_TESTS, losses / N_TESTS))
+    print(
+        "RANDOM TEST: %i / %i = %f; %i / %i = %f"
+        % (wins, N_TESTS, wins / N_TESTS, losses, N_TESTS, losses / N_TESTS)
+    )
+
 
 def test_verbose():
-
     while True:
-        env = Environment(0, 0, 1)
+        env = Environment(4)
         obs = env.observe()
         terminated = False
 
@@ -198,14 +235,15 @@ def test_verbose():
             action = agent.get_best_action(obs)
             next_obs, terminated = env.step_verbose(action)
             reward = agent.reward_function(obs, next_obs)
-            print("Reward: %f" %reward)
+            print("Reward: %f" % reward)
             input()
             obs = next_obs
 
+
 train()
 agent.print_strategy_table()
-#agent_graphs.plot_error(agent)
-#agent_graphs.plot_wins(win_count, checkpoint)
-#agent_graphs.plot_new_states(states, checkpoint)
+agent_graphs.plot_error(agent)
+agent_graphs.plot_wins(win_count, checkpoint)
+agent_graphs.plot_new_states(states, checkpoint)
 test()
 random_test()
